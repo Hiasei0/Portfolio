2294234159a19da496d57f0ca711086e5cfcd4f3
Luiz <98134886+Hiasei0@users.noreply.github.com>
2022-10-14 18:59:32 -0300

    Update and rename README.md to README.lua

---
+++ Scripts/README.lua
@@ -0,0 +1,19 @@
+--[[ License
+ ___   ___      ________       ________         ______         ______         ________
+/__/\ /__/\    /_______ /\    /_______/\       /_____ /\      /_____/\       /_______/\
+\::\ \\  \ \    \__.::._\/    \::: _  \ \       \::::_ \/     \::::_\/_      \__.::._\/
+ \::\/_\ .\ \        \::\ 	   \::(_)  \ \       \:\/___/\     \:\/___/\        \::\ \
+  \:: ___::\ \      _\::\ \__   \:: __  \ \       \_::._\:\     \::___\/_        \::\ \__
+   \: \ \\::\ \    /__\::\__/\   \:.\ \  \ \       /____\: \     \:\____/\     /__\::\__/\
+    \__\/ \::\/   \________\      \____\__\/       \_____\/       \_____\/    \___________
+]]
+
+
+**[Description]:**
+In this folder i will show to you some codes of my Portfolio
+
+**[Why Some?]:**
+I dont will open some codes here because have a high legion of peoples that will copy this or get from reference, i dont want that the objective be this.
+those that are more difficult, i will wait you send me a mensage requering.
+
+**Portfolio: [Working]**
--- Scripts/README.md
+++
@@ -1,7 +0,0 @@
-In this folder i will show to you some codes of my Portfolio
-
-**[Why Some?]:**
-I dont will open some codes here because have a high legion of peoples that will copy this or get from reference, i dont want that the objective be this.
-those that are more difficult, i will wait you send me a mensage requering.
-
-**Portfolio: [Working]**
